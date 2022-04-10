c4f9c847808d9b1626df7d7d572b459730fd9d28
Thomas TJ Dau <tj.pyro@gmail.com>
2022-04-10 17:13:12 -0500

    Removed debug statements

--- src/connection.py
+++ src/connection.py
@@ -22,14 +22,11 @@ class ScryfallConnection:
             r = await client.get(f"{self.url}{endpoint}", params=sorted(params))
             logger.info(f"GET {r.status_code}")
             sanitized = await self.sanitize_data(r.json())
-            # logger.debug(f"after sanitize {sanitized}")
             returnable = await self.pagination_list(return_data, sanitized)
-            # logger.debug(f"after pagination {returnable}")
             return returnable
 
     async def sanitize_data(self, data: Dict[str, Any] | List[Dict[str, Any]]) -> Dict[str, Any] | List[Dict[str, Any]]:
         if isinstance(data, list):
-            # logger.debug("Hit a list.")
             returnable = list()
             for item in data:
                 if isinstance(item, dict):
@@ -38,18 +35,13 @@ class ScryfallConnection:
                     returnable.append(item)
             return returnable
         elif isinstance(data, dict):
-            # logger.debug(data)
             if "id" in data:
-                # logger.debug("replacing 'id' with 'api_id' in data found.")
                 data["api_id"] = data.pop("id")
             if "object" in data:
-                # logger.debug("replacing 'object' with 'obj' in data found.")
                 data["obj"] = data.pop("object")
             returnable = dict()
             for k, v in data.items():
-                # logger.debug(f"Processing data {k}: {v}")
                 returnable[k] = await self.sanitize_data(v)
-            # logger.debug(returnable)
             return returnable
         else:
             return data
@@ -58,9 +50,7 @@ class ScryfallConnection:
         self, return_data: Optional[str], returnable: Dict[str, Any] | List[Dict[str, Any]]
     ) -> Dict[str, Any] | List[Dict[str, Any]]:
         if "has_more" in returnable:
-            # logger.debug("found has_more")
             if returnable["has_more"] and "next_page" in returnable:
-                # logger.debug("has_more true and has next_page")
                 url = returnable["next_page"].replace(self.url, "")
                 pagination = await self.get(url)
                 if return_data:
@@ -75,7 +65,6 @@ class ScryfallConnection:
         returnable_data = "data"
         bulk_data = await self.get("bulk-data", return_data=returnable_data)
         returnable = list()
-        # logger.debug(bulk_data)
         for item in bulk_data[returnable_data]:
             returnable.append(BulkData(**item))
         return returnable
