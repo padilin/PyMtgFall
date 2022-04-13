b8fd2206095002ad14e9062d496eb1ac6aa361cb
Thomas TJ Dau <tj.pyro@gmail.com>
2022-04-12 22:55:23 -0500

    Added first passing test.

--- tests/test_scryfall.py
+++ tests/test_scryfall.py
@@ -1,15 +1,32 @@
+from loguru import logger
+
 import httpx
 import pytest
+
+from httpx._exceptions import HTTPStatusError
 
 import src
 from src.connection import ScryfallConnection
 
 
 @pytest.mark.trio
-async def test_4xx_5xx_status(httpx_mock):
-    # httpx_mock.add_response()
-    #
-    # test_conn = ScryfallConnection()
-    # with pytest.raises(Exception) as execption_info:
-    #     await test_conn.get("symbology")
-    assert True
+async def test_get_4xx_5xx_triggers(httpx_mock, monkeypatch):
+    httpx_mock.add_response(
+        url="https://api.scryfall.com/symbology",
+        json={
+            "object": "error",
+            "code": "bad_request",
+            "status": 404,
+            "warnings": [
+                "Invalid expression “is:slick” was ignored. Checking if cards are “slick” is not supported",
+                "Invalid expression “cmc>cmc” was ignored. The sides of your comparison must be different.",
+            ],
+            "details": "All of your terms were ignored.",
+        },
+        status_code=404,
+    )
+
+    test_conn = ScryfallConnection()
+    with pytest.raises(HTTPStatusError) as e_info:
+        data = await test_conn.get("symbology")
+        logger.info(data)
